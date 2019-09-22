52d6d3874adfda97d1247ab51991a0547fec2b30
Abhay Kasavaraju <abhaykasavaraju@gmail.com>
2019-09-22 12:28:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/traffic/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hp.traffic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void viewComplaint(View view) {
+        Intent intent = new Intent(MainActivity.this, Complaint.class);
+        startActivity(intent);
+    }
+
+    public void updateStatus(View view) {
+        Intent intent = new Intent(MainActivity.this, Status.class);
+        startActivity(intent);
+    }
+}
